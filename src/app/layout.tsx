497eaa4fc4509a59e364a4a9229d696104891b6c
﻿Yh00meh <abbassiyamen2@gmail.com>
2026-05-08 14:33:37 -0400

    Added a light & dark mode feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
   variable: "--font-inter",
@@ -21,8 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={inter.variable} suppressHydrationWarning>
-        <body className={inter.className}>
+      <html lang="en" className={cn("font-sans", geist.variable)} suppressHydrationWarning>
+        <body className={cn("bg-background text-foreground", inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
           </ThemeProvider>
